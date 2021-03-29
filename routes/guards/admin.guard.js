1ded7ae9428992a66efa9670f7487239d1b80768
hussienibrahiem95 <hussienibrahiem1995@gmail.com>
2021-03-30 01:18:58 +0200

    edit

--- routes/guards/admin.guard.js
+++
@@ -1,7 +0,0 @@
-exports.isAuthAdmin = (req, res, next) => {
-    if (req.session.isAdmin) next()
-    else {
-        console.log('not admin');
-        res.redirect('/');
-    }
-}
--- routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const productController = require('../controllers/product.controller')
-
-router.get('/:id', productController.getProduct)
-
-module.exports = router;
